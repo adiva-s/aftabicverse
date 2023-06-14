e794cb46b1f40ad6f196de9d014cb8f3be3e1a2a
adivas <as13933@nyu.edu>
2023-06-14 00:41:13 -0400

    added Typing js library & animation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', function() {
     document.querySelector('.fade-in').classList.add('active');
-  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Typed.js initialization
+    var typed = new Typed('#typing', {
+        strings: [
+          'Welcome<br>to the<br>Aftab-verse'
+        ],
+        typeSpeed: 100,
+        showCursor: false
+    });
+});
+  
