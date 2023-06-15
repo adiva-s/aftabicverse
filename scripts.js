2f3425047fbe8427bc78f6ff4238ef5008436d4e
adivas <as13933@nyu.edu>
2023-06-15 00:50:46 -0400

    still working on nav bar, added new animation to index.html, editing about.html

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function() {
     document.querySelector('.fade-in').classList.add('active');
 });
 
+/*
 document.addEventListener('DOMContentLoaded', function() {
     // Typed.js initialization
     var typed = new Typed('#typing', {
@@ -12,4 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
         showCursor: false
     });
 });
-  
+*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Typed.js initialization for h1 element
+    var typed1 = new Typed('#typing', {
+      strings: [
+        'Welcome<br>to the<br>Aftab-verse'
+      ],
+      typeSpeed: 100,
+      showCursor: false,
+      onComplete: function() {
+        var h2Element = document.querySelector('.fade-up');
+        h2Element.classList.add('fade-up-active');
+      }
+    });
+  });
