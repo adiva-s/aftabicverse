6e5b9df9f11419fc866368323dacfd2466b30c92
Adiva <78193204+adiva-s@users.noreply.github.com>
2024-07-02 20:54:37 -0400

    added search bar to scripts.js

--- scripts.js
+++ scripts.js
@@ -46,20 +46,33 @@ function showSlide(n) {
   
   slides[slideIndex - 1].style.display = "block";
 }
-
 function searchIssues() {
-  let input = document.getElementById('searchInput').value.toUpperCase();
-  let issues = document.getElementsByClassName('issues-cover');
+  var input, filter, issues, issue, a, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  issues = document.getElementsByClassName('issues-cover');
   
-  for (let i = 0; i < issues.length; i++) {
-      let img = issues[i].getElementsByTagName("img")[0];
-      if (img) {
-          let txtValue = img.alt.toUpperCase();
-          if (txtValue.indexOf(input) > -1) {
-              issues[i].style.display = "";
+  for (i = 0; i < issues.length; i++) {
+      issue = issues[i].getElementsByTagName('a');
+      var match = false;
+
+      for (var j = 0; j < issue.length; j++) {
+          a = issue[j].getElementsByTagName('img')[0];
+          txtValue = a.alt || a.getAttribute('data-title') || "";
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              issue[j].style.display = "";
+              match = true;
           } else {
-              issues[i].style.display = "none";
+              issue[j].style.display = "none";
           }
-      }       
+      }
+
+      if (match) {
+          issues[i].style.display = "";
+      } else {
+          issues[i].style.display = "none";
+      }
   }
 }
+
+
